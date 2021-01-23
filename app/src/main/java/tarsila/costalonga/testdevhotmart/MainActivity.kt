726c8a9846b94a5b0fe10beb2698c2b7779e92bc
Társila Costalonga <tarsila.costalonga@gmail.com>
2021-01-23 11:40:13 -0300

    Criar a bottom navigation bar

--- app/src/main/java/tarsila/costalonga/testdevhotmart/MainActivity.kt
+++ app/src/main/java/tarsila/costalonga/testdevhotmart/MainActivity.kt
@@ -2,10 +2,14 @@ package tarsila.costalonga.testdevhotmart
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        navView.itemIconTintList = null
     }
 }
--- app/src/main/java/tarsila/costalonga/testdevhotmart/ui/home/HomeFragment.kt
+++ app/src/main/java/tarsila/costalonga/testdevhotmart/ui/home/HomeFragment.kt
@@ -42,4 +42,6 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+
 }
